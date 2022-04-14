6ef41eda1766df533c7c029a2020395671544a5c
Gabriel Henrique da Silva <gabriel.henrique@programmers.com.br>
2022-04-14 11:24:19 -0300

    Add Blog project

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return Ok();
+        }
+    }
+}
