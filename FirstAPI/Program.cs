dcfd34e35e5f9654fc468b07c491cdfdca3907bb
Gabriel Henrique da Silva <gabriel.henrique@programmers.com.br>
2022-04-14 08:43:27 -0300

    Add FirstAPI project

---
+++ FirstAPI/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapPost("/", (User user) => Results.Ok(user));
+
+app.Run();
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
